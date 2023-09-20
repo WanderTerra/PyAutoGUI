9d137ef5517af9438c6a82e27b3cb6954f3186dd
WanderTerra <105078842+WanderTerra@users.noreply.github.com>
2023-09-20 16:43:52 -0400

    Commit

---
+++ robo_mouse_teclado.py
@@ -0,0 +1,25 @@
+import pyautogui
+from time import sleep
+
+pyautogui.keyDown('alt')
+pyautogui.press('tab')
+pyautogui.keyUp('alt')
+
+sleep(1)
+pyautogui.click('Pesquisar.PNG')
+
+sleep(1)
+
+pyautogui.write('Enilda')
+
+sleep(2)
+
+pyautogui.click('Enilda.PNG')
+
+sleep(1)
+
+pyautogui.write('FAZ O L')
+
+sleep(1)
+
+# pyautogui.press('enter')
